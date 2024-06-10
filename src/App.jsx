8a8cff51983550ade087e5ff1ec108a75da6fb88
Muranguinh0 0 <annylamberti2006@gmail.com>
2024-06-10 05:07:38 +0000

    Syle: posicionamento do card no centro da tela, to passando mal real, marcio me ajuda

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 
-
 import { Home } from "./pages/Home"
 import { theme } from "./styles/theme.js"
 import { GlobalStyle } from "./styles/global.js"
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />
-  }
+  path: "/",
+  element: <Home/>
+}
 ])
 
 export function App() {
   return (
-
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
       <GlobalStyle />
-    </ThemeProvider> 
-    
+    </ThemeProvider>
   )
 }
---
+++ src/pages/Home.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const CardContainer = styled.div`
+    background: ${props => props.theme.cardGradient};
+    max-width: 41.2rem;
+    padding: 3.2rem;
+    border-radius: 3rem;
+`
+   
+export const IconContainer = styled.div``
+
+export const ButtonContainer = styled.div``
+
+export const ResultContainer = styled.p``
