d8bfaf9a55d23d85103cf6e14b5db576e3f0763e
Muranguinh0 0 <annylamberti2006@gmail.com>
2024-06-10 14:39:55 +0000

    style: estilização completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,27 +4,30 @@ import iconStarImg from "../assets/icon-star.svg"
 import thankYouImg from "../assets/illustration-thank-you.svg"
 import { useState } from "react";
 
-export function Home() {
-  return (
-    <CardContainer>
-      <IconContainer>
-        <img src={iconStarImg} alt="estrela" />
-      </IconContainer>
+  export function Home() {
+    return (
+          <CardContainer>
+            <IconContainer>
+              <img src={iconStarImg} alt="estrela" />
+            </IconContainer>
 
-      <h1>Como foi o atendimento?</h1>
-      <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+            <h1>Como foi o atendimento?</h1>
+            <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
 
-      <ButtonContainer>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-      </ButtonContainer>
+            <ButtonContainer>
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>4</button>
+              <button>5</button>
+            </ButtonContainer>
 
-      <button>Enviar</button>
-
-    </CardContainer>
-
-  )
-}
+            <button>Enviar</button>
+          </CardContainer>
+      ) : (
+          <CardContainer>
+            <img src={thankYouImg} alt="Imagem do agradecimento" />
+          </CardContainer>
+      )
+    )
+  }
